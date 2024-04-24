51821fd5ec5841deb72606661f087253dee6e7b0
Rafael Armenta <rarmenta@hawk.iit.edu>
2024-04-24 07:53:28 -0500

    Update animeChanApi.js

--- utils/animeChanApi.js
+++ utils/animeChanApi.js
@@ -1,24 +1,51 @@
-// src/app/utils/animeApi.js
+// utils/animeApi.js
 export const fetchRandomQuote = async () => {
-  const response = await fetch('https://animechan.xyz/api/random');
-  if (!response.ok) {
-    throw new Error('Failed to fetch random quote');
+  try {
+    const response = await fetch('https://animechan.xyz/api/random');
+    if (!response.ok) {
+      throw new Error('Failed to fetch random quote');
+    }
+    const data = await response.json();
+    return {
+      quote: data.quote,
+      character: data.character,
+      anime: data.anime,
+    };
+  } catch (error) {
+    throw error;
   }
-  return await response.json();
 };
 
 export const fetchQuoteByAnimeTitle = async (title) => {
-  const response = await fetch(`https://animechan.xyz/api/random/anime?title=${title}`);
-  if (!response.ok) {
-    throw new Error('Failed to fetch quote by anime title');
+  try {
+    const response = await fetch(`https://animechan.xyz/api/random/anime?title=${title}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch quote by anime title');
+    }
+    const data = await response.json();
+    return {
+      quote: data.quote,
+      character: data.character,
+      anime: data.anime,
+    };
+  } catch (error) {
+    throw error;
   }
-  return await response.json();
 };
 
 export const fetchQuoteByCharacter = async (name) => {
-  const response = await fetch(`https://animechan.xyz/api/random/character?name=${name}`);
-  if (!response.ok) {
-    throw new Error('Failed to fetch quote by character');
+  try {
+    const response = await fetch(`https://animechan.xyz/api/random/character?name=${name}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch quote by character');
+    }
+    const data = await response.json();
+    return {
+      quote: data.quote,
+      character: data.character,
+      anime: data.anime,
+    };
+  } catch (error) {
+    throw error;
   }
-  return await response.json();
 };
